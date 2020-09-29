1dbb40f519a161e71344f638cee937ade0c05f6b
William Herman <williamajherman@gmail.com>
2020-09-29 12:46:23 -0400

    add comment route

--- postRoutes.js
+++ postRoutes.js
@@ -21,6 +21,33 @@ router.post('/', (req, res) => {
 
 // post request for adding comment to a new post
 
+router.post('/:id/comments', (res, req) => {
+    const id = req.params.id;
+    const body = req.body;
+    if (body.text){
+        try {
+            const comment = {text: body.text, post_id: id};
+            db.insertComment(comment)
+                .then(res => {
+                    db.findCommentById(res.id)
+                        .then(res => {
+                            res.status(201).json({data: res});
+                        })
+                        .catch (err => {
+                            res.status(500).json({ error: "There was an error while saving the comment to the database" });
+                        });
+                })
+                .catch (err => {
+                    res.status(404).json({ message: "The post with the specified ID does not exist." });
+                });
+        } catch {
+            res.status(500).json({ error: "There was an error while saving the comment to the database" });
+        };
+    } else {
+        res.status(400).json({ errorMessage: "Please provide text for the comment." });
+    };
+});
+
 // get request for returning posts
 
 // get request for returning specific post
